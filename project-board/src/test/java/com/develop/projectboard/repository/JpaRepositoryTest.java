3e809e73f1e57b022c3ee8d7617fcd14fb1a8ced
ingwon97 <ghkdrnjs180@naver.com>
2023-03-18 16:20:49 +0900

    #8 - jpa test 작성

--- project-board/src/test/java/com/develop/projectboard/repository/JpaRepositoryTest.java
+++ project-board/src/test/java/com/develop/projectboard/repository/JpaRepositoryTest.java
@@ -1,11 +1,18 @@
 package com.develop.projectboard.repository;
 
 import com.develop.projectboard.config.JpaConfig;
+import com.develop.projectboard.domain.Article;
+import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
 import org.springframework.context.annotation.Import;
+import org.springframework.test.context.ActiveProfiles;
+
+import java.util.List;
+
+import static org.assertj.core.api.Assertions.*;
 
 @DisplayName("JPA 연결 테스트")
 @Import(JpaConfig.class)
@@ -20,7 +27,17 @@ public class JpaRepositoryTest {
         this.articleCommentRepository = articleCommentRepository;
     }
 
+    @DisplayName("select 테스트")
     @Test
-    void given_when_then() {
+    void givenTestData_whenSelecting_thenWorksFine() {
+
+        List<Article> articles = articleRepository.findAll();
+
+        //Then
+        assertThat(articles)
+                .isNotNull()
+                .hasSize(1000);
     }
+
+
 }
